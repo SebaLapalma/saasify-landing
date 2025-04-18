06549564b59fda43ca21bd709b0f5d21daf5dc74
sebalapalma <e.sebastian.lapalma@gmail.com>
2025-04-18 19:42:07 -0300

    algunos cambios a layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,27 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "SaaSify",
   description: "Modern SaaS landing page built with Next.js and Tailwind CSS.",
+  openGraph: {
+    title: "SaaSify - Landing Page Template",
+    description: "Una landing moderna, rápida y responsive para apps SaaS.",
+    url: "https://saasify-landing.vercel.app/",
+    siteName: "SaaSify",
+    images: [
+      {
+        url: "/og-preview-jpg",
+        width: 1200,
+        height: 630,
+        alt: "SaaSify preview",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SaaSify - Landing Page Template",
+    description: "Una landing moderna, rápida y responsive para apps SaaS.",
+    images: ['/og-preview.jpg'],
+  }
 };
 
 export default function RootLayout({ children }) {
